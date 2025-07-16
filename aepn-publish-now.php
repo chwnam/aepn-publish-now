3a7dff7c7e1efab8982eea68e964cc5fb9353b5a
changwoo <ep6tri@hotmail.com>
2025-07-17 00:09:38 +0900

    1.0.1 commit

--- aepn-publish-now.php
+++ aepn-publish-now.php
@@ -1,20 +1,21 @@
 <?php
 /**
  * Plugin Name: 바로 발행
- * Plugin URI:
- * Description: Threads attorneyengineer 님을 위한 플러그인. 블록 에디터에서 Ctrl+Alt+S 를 누르면 바로 발행하도록 동작합니다.
+ * Plugin URI: https://github.com/chwnam/aepn-publish-now
+ * Description: 블록 에디터에서 Ctrl+Shift+s를 누르면 바로 발행하도록 동작합니다.
  * Author: changwoo0215
  * Author URI: https://www.threads.com/@changwoo0215
- * Version: 1.0.0
+ * Version: 1.0.2
+ * License: GPLv2-or-later
  */
 
 if ( ! function_exists( 'aepn_init' ) ) {
 	function aepn_init(): void {
 		wp_register_script(
-			'aepn-direct-save',
+			'aepn-publish-now',
 			plugins_url( 'script.js', __FILE__ ),
 			[],
-			'1.0.0',
+			'production' === wp_get_environment_type() ? '1.0.2' : time(),
 			[
 				'strategy'  => 'defer',
 				'in_footer' => true,
@@ -27,8 +28,8 @@ add_action( 'init', 'aepn_init' );
 
 if ( ! function_exists( 'aepn_enqueue_scripts' ) ) {
 	function aepn_enqueue_scripts( string $hook ): void {
-		if ( 'post-new.php' === $hook) {
-			wp_enqueue_script( 'aepn-direct-save' );
+		if ( 'post-new.php' === $hook ||  'post.php' === $hook ) {
+			wp_enqueue_script( 'aepn-publish-now' );
 		}
 	}
 }
--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 (function () {
+    const clickPublish = () => {
+        const button = document.querySelector('button.editor-post-publish-button')
+        console.log(button)
+        if (button && !button.disabled) {
+            button.dispatchEvent(new MouseEvent('click', {
+                bubbles: true,
+                cancelable: true,
+                view: window,
+            }))
+            return true
+        }
+        return false
+    }
+
+    const openPanel = () => {
+        const panelButton = document.querySelector('button.editor-post-publish-panel__toggle')
+        if (panelButton && !panelButton.disabled) {
+            panelButton.dispatchEvent(new MouseEvent('click', {
+                bubbles: true,
+                cancelable: true,
+                view: window,
+            }))
+            return true
+        }
+        return false
+    }
+
     const eventHandler = (e) => {
         if (e.altKey && e.ctrlKey && 'KeyS' === e.code) {
-            const button = document.querySelector('button.editor-post-publish-button')
-            if (button && !button.disabled) {
-                button.dispatchEvent(new MouseEvent('click', {
-                    bubbles: true,
-                    cancelable: true,
-                    view: window,
-                }))
+            if (openPanel()) {
+                // Panel is open. Click publish button.
+                setTimeout(() => {
+                    clickPublish()
+                }, 1500)
+            } else {
+                // Panel is not present. Click publish button.
+                clickPublish()
             }
         }
     }
